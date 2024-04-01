1042b5e10a9fa476b28604a1863e7144f375dd1f
Cihan Bilgihan <cihanbilgihan@gmail.com>
2024-04-01 03:32:51 +0300

    Project Setup

---
+++ operator.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <type_traits>
+
+#if _HAS_CXX17
+#define _NAZ_INLINE inline
+#else // _HAS_CXX17
+#define _NAZ_INLINE 
+#endif // _HAS_CXX17
+
+namespace naz
+{
+	namespace detail
+	{
+		template <class _Fun>
+		struct operator_wrapper
+		{
+			_Fun fun;
+		};
+
+		template <class _Fun, class _Ty>
+		struct operator_wrapper_lhs
+		{
+			_Fun fun;
+			_Ty val;
+		};
+
+		template <class _Fun, class _Ty>
+		inline constexpr operator_wrapper_lhs<_Fun, _Ty&> operator<=(_Ty & left, const operator_wrapper<_Fun>&right)
+		{
+			return operator_wrapper_lhs<_Fun, _Ty&> { right.fun, left };
+		}
+
+		template <class _Fun, class _Ty>
+		inline constexpr operator_wrapper_lhs<_Fun, _Ty> operator<(_Ty left, const operator_wrapper<_Fun>&right)
+		{
+			return operator_wrapper_lhs<_Fun, _Ty> { right.fun, left };
+		}
+
+		template <class _Fun, class _Ty1, class _Ty2>
+		inline constexpr auto operator>=(const operator_wrapper_lhs<_Fun, _Ty1&>&left, _Ty2 & right)
+			-> decltype(left.fun(std::declval<_Ty1&>(), std::declval<_Ty2&>()))
+		{
+			return left.fun(left.val, right);
+		}
+
+		template <class _Fun, class _Ty1, class _Ty2>
+		inline constexpr auto operator>=(const operator_wrapper_lhs<_Fun, _Ty1>&left, _Ty2 & right)
+			-> decltype(left.fun(std::declval<_Ty1>(), std::declval<_Ty2&>()))
+		{
+			return left.fun(left.val, right);
+		}
+
+		template <class _Fun, class _Ty1, class _Ty2>
+		inline constexpr auto operator>(const operator_wrapper_lhs<_Fun, _Ty1>&left, _Ty2 right)
+			-> decltype(left.fun(std::declval<_Ty1>(), std::declval<_Ty2>()))
+		{
+			return left.fun(left.val, right);
+		}
+
+		template <class _Fun, class _Ty1, class _Ty2>
+		inline constexpr auto operator>(const operator_wrapper_lhs<_Fun, _Ty1&>&left, _Ty2 right)
+			-> decltype(left.fun(std::declval<_Ty1>(), std::declval<_Ty2>()))
+		{
+			return left.fun(left.val, right);
+		}
+	}
+
+	template <class _Fun>
+	inline constexpr detail::operator_wrapper<_Fun> make_operator(_Fun fun)
+	{
+		return detail::operator_wrapper<_Fun> { fun };
+	}
+
+}
+
+#define create_operator(operator_name, return_type, left_type, right_type) return_type __impl_of_ ## operator_name (left_type left, right_type right); _NAZ_INLINE constexpr auto operator_name = naz::make_operator(__impl_of_ ## operator_name); return_type __impl_of_ ## operator_name (left_type left, right_type right)
---
+++ test.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+#include "operator.h"
+
+create_operator(swap_op, void, int&, int&)
+{
+	int a = left;
+	left = right;
+	right = a;
+}
+
+create_operator(in_op, bool, char, const std::string&)
+{
+	return (right.find(left) != std::string::npos) ? true : false;
+}
+
+#define _swap_ <=swap_op>=
+#define is_in <in_op>
+
+int add_test(int l, int r)
+{
+	return l + r;
+}
+
+inline constexpr auto add_op = naz::make_operator(add_test);
+
+int main()
+{
+	int a = 0, b = 10;
+	a _swap_ b; // a == 10, b == 0;
+	std::cout << std::boolalpha << ('t' is_in "some string"); // prints -> true
+
+	std::cout << (5 < add_op > 10); // prints -> 15
+
+}
